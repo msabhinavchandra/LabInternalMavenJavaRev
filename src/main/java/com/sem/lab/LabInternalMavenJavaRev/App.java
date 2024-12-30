dc2ba3e813b04a7e0ba00ef337d7dfc90a7535e7
M.S.Abhinav Chandra <116635275+msabhinavchandra@users.noreply.github.com>
2024-12-30 11:57:10 +0530

    Update App.java

--- src/main/java/com/sem/lab/LabInternalMavenJavaRev/App.java
+++ src/main/java/com/sem/lab/LabInternalMavenJavaRev/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! after making changes here " );
+        System.out.println( "Hello World! after making changes here changes " );
     }
 }
